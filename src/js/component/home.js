45aa465d7506060bacd137e199088b52db082786
SilMontes <Silmontes2001@gmail.com>
2021-03-04 19:44:44 +0000

    Almost Done

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,27 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import { Navbar } from "./navbar"
+import { Jumbotron } from "./jumbotron";
+import { Cartas } from "./cartas"
+import  PropTypes from "prop-types";
 //create your first component
+
+let paragraphs={
+     firstParagraph:"Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.",
+    secondParagraph: "Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.",
+    thirdParagraph: "The sunflower (Helianthus annuus) is a living annual plant in the family Asteraceae, with a large flower head (capitulum).",
+    fourthParagraph: "The ermine is native to the area between the 40th parallel (north) and the beginning of the Arctic Circle."
+}
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container justify-content-center">
+			<Navbar />
+			<div>
+				<Jumbotron />
+				<Cartas firstParagraph/>
+			</div>
+            <footer className="footer">
+                <p>Copyrights Your Welcome 2019</p>
+            </footer>
 		</div>
 	);
 }
